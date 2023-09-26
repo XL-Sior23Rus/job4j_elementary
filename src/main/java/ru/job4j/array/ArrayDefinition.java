99e77766bc24fb2970bfcf46bd45ef40c39f2e4b
User <funkystore.krd@gmail.com>
2023-09-26 13:55:13 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,9 @@ public class ArrayDefinition {
     String[] surnames = new String[100500];
 
     float[] prices = new float[40];
+
+        System.out.println("Размер массива равен: " + numbers.length);
+        System.out.println("Размер массива равен: " + surnames.length);
+        System.out.println("Размер массива равен: " + prices.length);
     }
 }
