b81f682426bebf3a7a906e1d2ca48bbf07f1048b
User <funkystore.krd@gmail.com>
2023-09-26 14:10:26 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,15 +3,13 @@ package ru.job4j.array;
 public class ArrayDefinition {
 
     public static void main(String[] args) {
-
-    short[] numbers = new short[10];
-
-    String[] surnames = new String[100500];
-
-    float[] prices = new float[40];
-
-        System.out.println("Размер массива равен: " + numbers.length);
-        System.out.println("Размер массива равен: " + surnames.length);
-        System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Vladimir Zapashniy";
+        names[1] = "Elena Zapashnaa";
+        names[2] = "Irina Zapashnaa";
+        names[3] = "Aleksandr Zapashniy";
+            System.out.println(names[0]);
+        }
     }
-}
+
+
