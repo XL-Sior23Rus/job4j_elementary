ee56ea3cad620515719b0a6a5755040369ee5fd8
User <funkystore.krd@gmail.com>
2023-09-26 11:07:27 +0300

    9.1. String [#237407 #446479]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 public class Greeting {
+
     public static void main(String[] args) {
         String idea = "I like Java";
         idea = idea + " But i am a newbie";
